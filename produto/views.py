f9fbbf2c9a78e48a4b26069d8e39aeb4b5b2dc54
Diego Vanderlei da Silva Hoffmann <diegohoffmann4@gmail.com>
2021-06-08 21:09:41 +0200

    views

--- produto/views.py
+++ produto/views.py
@@ -1,3 +1,36 @@
+from django import views
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+
+class DetalheProduto(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
+
+
+class AdicionarAoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Adicionar ao carrinho')
+
+
+class RemoverDoCarrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Remover do carrinho')
+
+
+class Carrinho(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Carrinho')
+
+
+class Finalizar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Finalizar')
